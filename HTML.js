560a21daa1cfef16d1ba544a3352a3b10868971a
Taisuke Fukuno <fukuno@jig.jp>
2023-09-26 22:36:11 +0900

    Initial commit

---
+++ HTML.js
@@ -0,0 +1,72 @@
+const spchars = {
+  gt: ">",
+  lt: "<",
+  amp: "&",
+  times: "×",
+  quot: "\"",
+  reg: "®",
+  copy: "©",
+  rsquo: "’",
+  lsquo: "‘",
+  sbquo: "“",
+  rdquo: "”",
+  deg: "°",
+  nbsp: " ",
+  hookleftarrow: "↩",
+};
+
+export const HTML = {
+  decode(s) {
+    const n = s.indexOf("&");
+    if (n < 0) {
+      return s;
+    }
+    const m = s.indexOf(";", n + 1);
+    if (m < 0) {
+      return s;
+    }
+
+    const res = [];
+    const sp = s.substring(n + 1, m);
+    const c = spchars[sp];
+    let p = m + 1;
+    if (!c) {
+      console.log("unsupported special char", sp);
+      res.push(s.substring(0, n + 1));
+      p = n + 1;
+    } else {
+      res.push(s.substring(0, n));
+      res.push(c);
+    }
+    for (;;) {
+      const n = s.indexOf("&", p);
+      if (n < 0) {
+        res.push(s.substring(p));
+        break;
+      }
+      const m = s.indexOf(";", n + 1);
+      if (m < 0) {
+        res.push(s.substring(p));
+        break;
+      }
+      const sp = s.substring(n + 1, m);
+      const c = spchars[sp];
+      if (!c) {
+        console.log("unsupported special char", sp);
+        res.push(s.substring(p, n + 1));
+        p = n + 1;
+      } else {
+        res.push(s.substring(p, n));
+        res.push(c);
+        p = m + 1;
+      }
+    }
+    return res.join("");
+  },
+  encode(s) {
+    return s.replace(/&/g, "&amp;");
+  },
+  getSpecialChars() {
+    return spchars;
+  }
+};
---
+++ HTML.test.js
@@ -0,0 +1,29 @@
+import * as t from "https://deno.land/std/testing/asserts.ts";
+import { HTML } from "./HTML.js";
+
+Deno.test("encode", () => {
+  t.assertEquals(HTML.encode("abc"), "abc");
+  t.assertEquals(HTML.encode("a&bc"), "a&amp;bc");
+  t.assertEquals(HTML.encode("a&b&c"), "a&amp;b&amp;c");
+  t.assertEquals(HTML.encode("a&&c"), "a&amp;&amp;c");
+});
+Deno.test("decode", () => {
+  t.assertEquals(HTML.decode("abc"), "abc");
+  t.assertEquals(HTML.decode("a&amp;bc"), "a&bc");
+  t.assertEquals(HTML.decode("a&amp;b&amp;c"), "a&b&c");
+  t.assertEquals(HTML.decode("a&amp;&amp;c"), "a&&c");
+});
+Deno.test("decode < >", () => {
+  t.assertEquals(HTML.decode("&gt;"), ">");
+  t.assertEquals(HTML.decode("&lt;&gt;"), "<>");
+  t.assertEquals(HTML.decode("&lt;&gt;"), "<>");
+});
+Deno.test("special chars", () => {
+  //console.log(Object.keys(HTML.getSpecialChars()).map(s => "&" + s + ";").join(""));
+  //console.log(Object.values(HTML.getSpecialChars()).join(""));
+  t.assertEquals(HTML.decode("&gt;&lt;&amp;&times;&quot;&reg;&copy;&rsquo;&lsquo;&sbquo;&rdquo;&deg;&nbsp;&hookleftarrow;"), `><&×"®©’‘“”° ↩`)
+});
+Deno.test("decode no defines", () => {
+  t.assertEquals(HTML.decode("&aa;"), "&aa;");
+  t.assertEquals(HTML.decode("&ab&aa;"), "&ab&aa;");
+});
